7cc93071a5e9c7dde036f060c059249cfd4aab14
HarshadJariwala <harshadjariwala1998@gmail.com>
2026-01-02 19:59:08 +0530

    update

--- src/styles/image.js
+++ src/styles/image.js
@@ -2,7 +2,8 @@ import DEFAULTSPLASH from '../../src/assets/images/defaultsplash.jpg';
 import COURSEIMAGE from '../../src/assets/images/courseimage.png';
 import LOGINICON from '../../src/assets/images/loginicon.png';
 import SHIKSHALOGO from '../../src/assets/images/shikshalogo.png';
+import DASHBOARDHEADER from '../../src/assets/images/dashboardheader.png';
 
 export {
-    DEFAULTSPLASH, COURSEIMAGE, LOGINICON, SHIKSHALOGO
+    DEFAULTSPLASH, COURSEIMAGE, LOGINICON, SHIKSHALOGO, DASHBOARDHEADER
 };
